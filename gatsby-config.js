52e3a1099e46a2aedf7ce3de93aef2968ee0425a
Tom Snelling <tomsnelling8@gmail.com>
2019-06-04 11:00:08 +0100

    use `noopener noreferrer` on remark links

--- gatsby-config.js
+++ gatsby-config.js
@@ -49,7 +49,7 @@ module.exports = {
             resolve: 'gatsby-remark-external-links',
             options: {
               target: '_blank',
-              rel: 'nofollow',
+              rel: 'noopener noreferrer',
             },
           },
         ],
